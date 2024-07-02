0e9ba26b4874974f88ecdb9e7892a8addb5513df
ADITI <aditi.shukla01999@gmail.com>
2024-07-02 23:35:34 +0530

    Update Tags.jsx -removed unwanted comments

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -6,13 +6,14 @@ import { styled } from "@mui/material/styles";
 
 import Button from "@mui/material/Button";
 
-// import pattern from "./assets/denim.webp"
+
 import ClearIcon from '@mui/icons-material/Clear';
 
 
 export default function Tags({selectedFilters,handleState}){
     const font =  '"Margarine", sans-serif';
     let {genderTrue,originTrue,speciesTrue,statusTrue}=selectedFilters;
+
     const MainHeadings= styled(Typography)(() => ({
         fontFamily:font,textAlign:"left",paddingBottom:"0.5rem",
         fontSize:"2rem"
@@ -38,16 +39,16 @@ export default function Tags({selectedFilters,handleState}){
     <Box sx={{height:"20vh",display:"flex",flexDirection:"row",justifyContent:"flex-start",alignItems:"flex-start"}}>
     
      {originTrue?.length>0 && originTrue.map((el,i)=>{
-       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("origin",false,el)} endIcon={<ClearIcon/>}>{el}</CustomButton>
+       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("origin",el)} endIcon={<ClearIcon/>}>{el}</CustomButton>
      })}
      {genderTrue?.length>0 && genderTrue.map((el,i)=>{
-       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("gender",false,el)}  endIcon={<ClearIcon/>}>{el}</CustomButton>
+       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("gender",el)}  endIcon={<ClearIcon/>}>{el}</CustomButton>
      })}
      {speciesTrue?.length>0 && speciesTrue.map((el,i)=>{
-       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("species",false,el)}  endIcon={<ClearIcon/>}>{el}</CustomButton>
+       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("species",el)}  endIcon={<ClearIcon/>}>{el}</CustomButton>
      })}
      {statusTrue?.length>0 && statusTrue.map((el,i)=>{
-       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("status",false,el)}  endIcon={<ClearIcon/>}>{el}</CustomButton>
+       return     <CustomButton variant="contained" key={i} onClick={()=>handleState("status",el)}  endIcon={<ClearIcon/>}>{el}</CustomButton>
      })}
      </Box>
      </Box>)
